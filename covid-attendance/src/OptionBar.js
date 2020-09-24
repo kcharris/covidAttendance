e4f8fac25df1cc4b5b6325d1b275a6bddf9d55d2
Kevin <kcharris77@gmail.com>
2020-09-24 00:11:41 -0500

    remove study note and format style format files

--- covid-attendance/src/OptionBar.js
+++ covid-attendance/src/OptionBar.js
@@ -1,13 +1,14 @@
 import React from "react";
 
-class OptionBar extends React.Component{
-    
-    render(){
-        return(
-        <div>
-            <button onClick={this.props.onAClick} style={{backgroundColor:"lightgreen"}}>Attendance</button>
-            <button onClick={this.props.onGClick} style={{backgroundColor:"lightblue"}}>Groups</button>
-        </div>
-        )}
+class OptionBar extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.props.onAClick} style={{ backgroundColor: "lightgreen" }}>Attendance</button>
+                <button onClick={this.props.onGClick} style={{ backgroundColor: "lightblue" }}>Groups</button>
+            </div>
+        )
+    }
 }
 export default OptionBar
--- covid-attendance/src/Overview.js
+++ covid-attendance/src/Overview.js
@@ -2,17 +2,9 @@ import React from "react";
 import OptionBar from "./OptionBar"
 import Student from "./Student"
 
-/*
-originally, I tried creating both students and the output array containing the <student> component list inside of setState. This did not work because student was not defined
-Then, I tried moving messing around with functions to no avail. I knew the syntax sort of well but I kept trying
-Then when that failed I tried to move both into functions that would run when the program begins to output the table without having to double click, sort of. There were multiple
-Then, after messing around for a while I got it to work, component did mount, in addition to moving the initial setState of a student fill function allowed my to create
-a output format.
-I still had an update issue. That was fixed with component will update after figuring out I needed both parameters to specify the precState, which was the second.
-Finally, at least for this problem, I removed the component did mount and component did update in favor of list that rerenders every time the render is called.
-*/
-class Overview extends React.Component{
-    constructor(props){
+
+class Overview extends React.Component {
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -20,7 +12,7 @@ class Overview extends React.Component{
             groupColor: 'blue',
             students: (() => {
                 let newDict = []
-                for(let i = 0; i < 20; i++){
+                for (let i = 0; i < 20; i++) {
                     newDict.push(
                         {
                             name: `student${i + 1}`,
@@ -32,7 +24,7 @@ class Overview extends React.Component{
                 return newDict
             })(),
         }
-        
+
         this.handleAttendanceClick = this.handleAttendanceClick.bind(this);
         this.handleGroupClick = this.handleGroupClick.bind(this);
         this.handleStudentGroupClick = this.handleStudentGroupClick.bind(this);
@@ -47,40 +39,41 @@ class Overview extends React.Component{
             groupBool: true
         })
     }
-    handleStudentGroupClick(studentId){
+    handleStudentGroupClick(studentId) {
         this.setState((state) => {
-            return({
+            return ({
                 students: (() => {
                     let newStudents = state.students.map((student) => {
-                        if(student.id === studentId){
+                        if (student.id === studentId) {
                             let newStudent = student
                             newStudent.color = this.state.groupColor
                             return newStudent
                         }
-                        else{
+                        else {
                             return student
                         }
                     })
-                    
+
                     return newStudents
                 })(),
             })
         })
     }
 
-    render(){
+    render() {
         let nList = []
-        if(this.state.students.length !== 0){
+        if (this.state.students.length !== 0) {
             for (let i = 0; i < this.state.students.length; i++) {
-                nList.push(<Student student={this.state.students[i]} gBool={this.state.groupBool} key={this.state.students[i].id} handleStudentGroupClick={this.handleStudentGroupClick}/>)
+                nList.push(<Student student={this.state.students[i]} gBool={this.state.groupBool} key={this.state.students[i].id} handleStudentGroupClick={this.handleStudentGroupClick} />)
             }
         }
-    
-        return(           
+
+        return (
             <div>
-                <OptionBar onAClick={this.handleAttendanceClick} onGClick={this.handleGroupClick}/>
+                <OptionBar onAClick={this.handleAttendanceClick} onGClick={this.handleGroupClick} />
                 {nList}
             </div>
-        )}
+        )
+    }
 }
 export default Overview
