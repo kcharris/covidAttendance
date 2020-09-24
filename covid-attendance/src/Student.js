81f9cffc1bcb56ab0271e4bb70e070765f7cbc0d
Kevin <kcharris77@gmail.com>
2020-09-24 13:40:38 -0500

    add way to switch attendance colors between red and green

--- covid-attendance/src/Student.js
+++ covid-attendance/src/Student.js
@@ -1,11 +1,23 @@
 import React from "react";
 
 class Student extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            absent: false,
+        }
+        this.changeAttendance = this.changeAttendance.bind(this)
+    }
+    changeAttendance(){
+        this.setState({
+            absent: !this.state.absent
+        })
+    }
     
     render(){
         let button = <div></div>
         if(!this.props.gBool){
-            button = <button style={{backgroundColor: "lightgreen"}}>{this.props.student.name}</button>
+            button = <button onClick={this.changeAttendance} style={this.state.absent ? {backgroundColor: "red"}: {backgroundColor: "lightgreen"}}>{this.props.student.name}</button>
         }
         else{
             button = <button onClick={() => this.props.handleStudentGroupClick(this.props.student.id)} style={{backgroundColor: this.props.student.color}}>{this.props.student.name}</button>
