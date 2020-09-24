715baf092fa5b6e40a1d58ca4108ed2d309fb863
Kevin <kcharris77@gmail.com>
2020-09-24 14:06:52 -0500

    Updates

---
+++ precache-manifest.5fc93266c9ed09e64e3839f3fe2284bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b94da55c0eb9e32250fe9917eb1b97c2",
+    "url": "/covidAttendance/index.html"
+  },
+  {
+    "revision": "a05ceba466421ce159a0",
+    "url": "/covidAttendance/static/css/main.9a6ccec5.chunk.css"
+  },
+  {
+    "revision": "b1c9a6b130fc038778fd",
+    "url": "/covidAttendance/static/js/2.ac2957a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covidAttendance/static/js/2.ac2957a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a05ceba466421ce159a0",
+    "url": "/covidAttendance/static/js/main.cd1fd1f5.chunk.js"
+  },
+  {
+    "revision": "30148e62e8465d101329",
+    "url": "/covidAttendance/static/js/runtime-main.850434ad.js"
+  }
+]);
